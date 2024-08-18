c6d81a81b736072d78d8b5dbbdec7ec026847dab
IacobucciB <link00222@gmail.com>
2024-08-18 11:48:45 -0300

    leet 26 init

---
+++ leetcode/026-remove-duplicates-from-sorted-array/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        print(nums)
+        return 1
+
+
+sol = Solution()
+test = [0,0,1,1,1,2,2,3,3,4]
+k = sol.removeDuplicates(test)
+print(k)
