0d3d9310695412a49b6df73403a7b041aefb31a7
Iacobucci Juan B. <link00222@gmail.com>
2024-08-18 23:02:08 -0300

    leet 26 done

--- leetcode/026-remove-duplicates-from-sorted-array/main.py
+++ leetcode/026-remove-duplicates-from-sorted-array/main.py
@@ -3,21 +3,13 @@ class Solution:
         if not nums:
             return 0
         k = 1
-        for i in range(len(nums)-1,-1,-1):
-            if nums[i] == nums[i-1]:
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
                 k += 1
-                nums[i] = "_"
-                
-        for _ in range(k):
-            for i in range(len(nums)-1,0,-1):
-                if nums[i-1] == "_":
-                    nums[i-1] = nums[i]
-                    nums[i] = "_"
-        print(nums)
         return k
 
-
 sol = Solution()
-test = [1,1,2]
+test = [0,0,1,1,1,2,2,3,3,4]
 k = sol.removeDuplicates(test)
 print(k)
