7af9871d5436c96cf649ed31924c682f609fb2f2
Iacobucci Juan B. <link00222@gmail.com>
2025-01-12 21:47:45 -0300

    Add C++ implementation for Two Sum problem

---
+++ leetcode/001-two-sum/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = solution.twoSum(nums, target);
+    
+    if (!result.empty()) {
+        std::cout << "Indices: " << result[0] << ", " << result[1] << std::endl;
+    } else {
+        std::cout << "No solution found." << std::endl;
+    }
+
+    return 0;
+}
