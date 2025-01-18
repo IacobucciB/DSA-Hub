73a59f15144fc96a7825ff714d47cd9544d7d9d1
IacobucciB <link00222@gmail.com>
2025-01-18 11:13:16 -0300

    Start C++ solution for Euler Problem 3

--- euler/problem-3/main.cpp
+++ euler/problem-3/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+int trial_division(int number) {
+    return 1;
+}
+
 int main(void) {
 
-
+    long long int number = 600851475143;
+    long long int result = trial_division(number);
+    cout << result << endl;
     return 0;
 }
