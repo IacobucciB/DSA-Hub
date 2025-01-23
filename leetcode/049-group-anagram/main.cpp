29386c7f0b74598452712e2ac50a9f35261bbfae
Iacobucci Juan B. <link00222@gmail.com>
2025-01-22 22:13:19 -0300

    Add initial C++ implementation for Group Anagrams solution

---
+++ leetcode/049-group-anagram/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+    }
+};
+
+int main() {
+
+    cout << "Hello o7" << endl;
+    
+    return 0;
+}
