6f182991e224c58738ec0751de14e672d93f7aea
IacobucciB <link00222@gmail.com>
2025-02-09 09:37:01 -0300

    euler tidy

--- euler/problem-3/main.py
+++ euler/problem-3/main.py
@@ -1,17 +1,2 @@
-def trial_division(n: int) -> list[int]:
-    a = []
-    f = 2
-
-    while f*f <= n:
-        if n % f == 0:
-            a.append(f)
-            n //= f
-        else:
-            f += 1
-    if n != 1:
-        a.append(n)
-    return a
-
-number: int = 600851475143
-res = trial_division(number)
-print(res)
+number = 600851475143
+print(number)
