9badb4309233435d5aec461e1c2754ced7b99ca1
IacobucciB <link00222@gmail.com>
2025-03-11 10:49:12 -0300

    init: leetcode 238 productExceptSelf

---
+++ leetcode/238-productExceptSelf/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        print("Hello world")
+
+nums = [1,2,3,4]
+s = Solution()
+s.productExceptSelf(nums)
